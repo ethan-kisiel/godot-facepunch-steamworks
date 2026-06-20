5f2d21fdcde0d2b7af690f549a0a3af3af143ee4
ethan-kisiel <ethan.a.kisiel@gmail.com>
2026-06-20 10:31:38 -0500

    update godot steam packet unsafe to use managed memory

---
+++ core/domain/backup/GodotSteamPacketUnsafe.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Runtime.InteropServices;
+using Steamworks.Data;
+
+namespace facepunchsteamworkstest.core.domain.steam_multiplayer;
+
+public class GodotSteamPacketUnsafe: IDisposable
+{
+    static readonly int HeaderSize = sizeof(MessageTypeEnum) + sizeof(SendType) + sizeof(int);
+
+    private IntPtr _packetData;
+    private bool disposed;
+
+    public MessageTypeEnum MessageType { get; set; }
+    public IntPtr Payload { get; set; }
+    public int PayloadSize { get; set; }
+    public int Channel { get; set; }
+    public int From { get; set; }
+    public SendType SendType { get; set; }
+
+    public int Size { get; private set; }
+    
+    public static GodotSteamPacketUnsafe Decode(IntPtr buffer, int payloadSize, ushort channel = 0)
+    {
+        GodotSteamPacketUnsafe godotSteamPacket = new GodotSteamPacketUnsafe();
+        godotSteamPacket.MessageType = (MessageTypeEnum)Marshal.ReadByte(buffer);
+        godotSteamPacket.SendType = (SendType)Marshal.ReadInt32(buffer, 1);
+        godotSteamPacket.From = Marshal.ReadInt32(buffer, 5);
+        godotSteamPacket.Payload = Marshal.ReadIntPtr(buffer, 9);
+        godotSteamPacket.Channel = channel;
+        godotSteamPacket.PayloadSize = payloadSize;
+
+        godotSteamPacket.Size = HeaderSize + payloadSize;
+
+        return godotSteamPacket;
+    }
+
+    public IntPtr Encode()
+    {
+        IntPtr buffer = Marshal.AllocHGlobal(Size);
+        Marshal.WriteByte(buffer, (byte)MessageType);
+        Marshal.WriteInt32(buffer, 1, (int)SendType);
+        Marshal.WriteInt32(buffer, 5, From);
+        Marshal.WriteIntPtr(buffer, Payload);
+
+        if (_packetData != IntPtr.Zero)
+        {
+            Marshal.FreeHGlobal(_packetData);
+        }
+
+        _packetData = buffer;
+
+        return buffer;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (_packetData != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(_packetData);
+            }
+            Payload = IntPtr.Zero;
+            disposed = true;
+        }
+    }
+    ~GodotSteamPacketUnsafe()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
